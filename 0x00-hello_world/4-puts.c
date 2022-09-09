0f4bcd14dc27e9b64b4281059bae123e3ef386f9
Esimylove <Helewosi25@gmail.com>
2022-09-08 20:21:01 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 <stdio.h>
-
 /**
- * * main - shown print
- * *
- * * Return: Always 0 (done)
- * */
+ * main - shown print
+ *
+ * Return: Always 0 (done)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
